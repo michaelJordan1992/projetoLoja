64b0858f803f9cec3765ce7d5e3783d3880a9511
michaelJordan1992 <michael_jordan@outlook.com.br>
2018-11-07 14:37:58 -0200

    Classe de interação Visual / Banco de dados

---
+++ src/Controller/ClienteController.java
@@ -0,0 +1,58 @@
+package Controller;
+
+import DAO.DAOCliente;
+import Model.Cliente;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author michael
+ */
+public class ClienteController {
+    DAOCliente dc;
+    
+    public void cadastrarCliente(Cliente c) throws Exception{
+        dc = new DAOCliente();
+        dc.CadastrarCliente(c);
+        JOptionPane.showMessageDialog(
+        null,
+                "Cliente Cadastrado com Sucesso!!!");
+    }
+    public void deletarClienteId(Cliente c) throws Exception{
+        dc = new DAOCliente();
+        dc.deletarClienteId(c);
+        JOptionPane.showMessageDialog(
+        null,
+                "Cliente Deletado!! ");
+    }
+    public void deletarClienteCpf(Cliente c) throws Exception{
+        dc = new DAOCliente();
+        dc.deletarClienteCpf(c);
+        JOptionPane.showMessageDialog(
+        null,
+                "Cliente Deletado!! ");
+    }
+    public void deletarClienteRg(Cliente c) throws Exception{
+        dc = new DAOCliente();
+        dc.deletarClienteRg(c);
+        JOptionPane.showMessageDialog(
+        null, 
+                "Cliente Deletado!!");
+    }
+    public ArrayList<Cliente> buscarTodosClientes() throws Exception{
+        dc = new DAOCliente();
+        return dc.buscarTodosClientes();
+    }
+    public ArrayList<Cliente> buscarClienteCpf(Cliente c) throws Exception{
+        dc = new DAOCliente();
+        return dc.buscarClienteCpf(c);
+    }
+    public void atualizarClienteCpf(Cliente c) throws Exception{
+        dc = new DAOCliente();
+        dc.atualizarClienteCpf(c);
+        JOptionPane.showMessageDialog(
+        null, 
+                "Cliente Alterado com Sucesso!!");
+    }
+}
