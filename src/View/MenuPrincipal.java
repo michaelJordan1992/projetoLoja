fb2971079559ee4be4b7ea9d5223dd5a6c877009
michaelJordan1992 <michael_jordan@outlook.com.br>
2018-11-08 23:01:28 -0200

    Menu Principal

--- src/View/MenuPrincipal.java
+++ src/View/MenuPrincipal.java
@@ -23,6 +23,7 @@ public class MenuPrincipal extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
+        j_AreaTrabalho = new javax.swing.JDesktopPane();
         jmb_Menu = new javax.swing.JMenuBar();
         jm_Cadastro = new javax.swing.JMenu();
         jmi_CadastroCliente = new javax.swing.JMenuItem();
@@ -44,13 +45,37 @@ public class MenuPrincipal extends javax.swing.JFrame {
         jmi_BuscarTodosClientes = new javax.swing.JMenuItem();
         jmi_BuscarTodosProdutos = new javax.swing.JMenuItem();
         jmi_BuscarTodosVendedores = new javax.swing.JMenuItem();
+        jm_Venda = new javax.swing.JMenu();
+        jmi_AbrirVenda = new javax.swing.JMenuItem();
+        jmi_BuscaVendaGeral = new javax.swing.JMenuItem();
+        jmi_BuscarVendaIndividual = new javax.swing.JMenuItem();
+        jmi_DeletarVenda = new javax.swing.JMenuItem();
+        jmi_AtualizarVenda = new javax.swing.JMenuItem();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setTitle("Loja da Mariazinha");
 
+        j_AreaTrabalho.setBackground(new java.awt.Color(204, 204, 255));
+
+        javax.swing.GroupLayout j_AreaTrabalhoLayout = new javax.swing.GroupLayout(j_AreaTrabalho);
+        j_AreaTrabalho.setLayout(j_AreaTrabalhoLayout);
+        j_AreaTrabalhoLayout.setHorizontalGroup(
+            j_AreaTrabalhoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 760, Short.MAX_VALUE)
+        );
+        j_AreaTrabalhoLayout.setVerticalGroup(
+            j_AreaTrabalhoLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 487, Short.MAX_VALUE)
+        );
+
         jm_Cadastro.setText("Cadastro");
 
         jmi_CadastroCliente.setText("Cliente");
+        jmi_CadastroCliente.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jmi_CadastroClienteActionPerformed(evt);
+            }
+        });
         jm_Cadastro.add(jmi_CadastroCliente);
 
         jmi_CadastrarProduto.setText("Produto");
@@ -113,21 +138,46 @@ public class MenuPrincipal extends javax.swing.JFrame {
 
         jmb_Menu.add(jm_BuscaTodos);
 
+        jm_Venda.setText("Venda");
+
+        jmi_AbrirVenda.setText("Abrir Nova Venda");
+        jm_Venda.add(jmi_AbrirVenda);
+
+        jmi_BuscaVendaGeral.setText("Buscar Venda");
+        jm_Venda.add(jmi_BuscaVendaGeral);
+
+        jmi_BuscarVendaIndividual.setText("Buscar Venda Individual");
+        jm_Venda.add(jmi_BuscarVendaIndividual);
+
+        jmi_DeletarVenda.setText("Deletar Venda");
+        jm_Venda.add(jmi_DeletarVenda);
+
+        jmi_AtualizarVenda.setText("Atualizar Venda");
+        jm_Venda.add(jmi_AtualizarVenda);
+
+        jmb_Menu.add(jm_Venda);
+
         setJMenuBar(jmb_Menu);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 636, Short.MAX_VALUE)
+            .addComponent(j_AreaTrabalho, javax.swing.GroupLayout.Alignment.TRAILING)
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 423, Short.MAX_VALUE)
+            .addComponent(j_AreaTrabalho)
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void jmi_CadastroClienteActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jmi_CadastroClienteActionPerformed
+        GuiCadastroCliente gcc = new GuiCadastroCliente();
+        gcc.setVisible(true);
+        j_AreaTrabalho.add(gcc);
+    }//GEN-LAST:event_jmi_CadastroClienteActionPerformed
 
     /**
      * @param args the command line arguments
@@ -165,26 +215,33 @@ public class MenuPrincipal extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JDesktopPane j_AreaTrabalho;
     private javax.swing.JMenu jm_Alterar;
     private javax.swing.JMenu jm_BuscaSeparada;
     private javax.swing.JMenu jm_BuscaTodos;
     private javax.swing.JMenu jm_Cadastro;
     private javax.swing.JMenu jm_Deletar;
+    private javax.swing.JMenu jm_Venda;
     private javax.swing.JMenuBar jmb_Menu;
+    private javax.swing.JMenuItem jmi_AbrirVenda;
     private javax.swing.JMenuItem jmi_AlterarCliente;
     private javax.swing.JMenuItem jmi_AlterarProduto;
     private javax.swing.JMenuItem jmi_AlterarVendedor;
+    private javax.swing.JMenuItem jmi_AtualizarVenda;
+    private javax.swing.JMenuItem jmi_BuscaVendaGeral;
     private javax.swing.JMenuItem jmi_BuscarCliente;
     private javax.swing.JMenuItem jmi_BuscarProduto;
     private javax.swing.JMenuItem jmi_BuscarTodosClientes;
     private javax.swing.JMenuItem jmi_BuscarTodosProdutos;
     private javax.swing.JMenuItem jmi_BuscarTodosVendedores;
+    private javax.swing.JMenuItem jmi_BuscarVendaIndividual;
     private javax.swing.JMenuItem jmi_BuscarVendedor;
     private javax.swing.JMenuItem jmi_CadastrarProduto;
     private javax.swing.JMenuItem jmi_CadastrarVendedor;
     private javax.swing.JMenuItem jmi_CadastroCliente;
     private javax.swing.JMenuItem jmi_DeletarCliente;
     private javax.swing.JMenuItem jmi_DeletarProduto;
+    private javax.swing.JMenuItem jmi_DeletarVenda;
     private javax.swing.JMenuItem jmi_DeletarVendedor;
     // End of variables declaration//GEN-END:variables
 }
