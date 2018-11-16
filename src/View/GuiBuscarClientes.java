c866af4abd400dd6c3cfe09b893524ad95ad3d82
michaelJordan1992 <michael_jordan@outlook.com.br>
2018-11-16 17:17:41 -0200

    Tela de Busca de Todos Clientes

--- src/View/GuiBuscarClientes.java
+++ src/View/GuiBuscarClientes.java
@@ -12,27 +12,31 @@ import javax.swing.table.DefaultTableModel;
  * @author michael
  */
 public class GuiBuscarClientes extends javax.swing.JInternalFrame {
+    
     DefaultTableModel dtm = new DefaultTableModel(new Object[][]{},
     new Object[]{"ID","NOME","CPF","RG","CELULAR","ENDERECO","CIDADE","ESTADO",});
     
     public GuiBuscarClientes() {
-        super ("Pesquisar Pessoas");
+        super ("Pesquisar Cliente");
         initComponents();
+        this.buscarTodosCLientes();
     }
     private void buscarTodosCLientes(){
         try{
             tabela_Cliente.setModel(dtm);
             ClienteController cc = new ClienteController();
             ArrayList<Cliente> cliente = new ArrayList<>();
-            cc.buscarTodosClientes();
-            dtm.setNumRows(8);
+            cliente = cc.buscarTodosClientes();
             
+            dtm.setNumRows(0);
+                        
             for(int i = 0; i < cliente.size(); i++){
-                dtm.addRow(new String[]{
-                    
+                dtm.addRow(new Object[]{    
+                    Integer.toString(cliente.get(i).getIdCliente()),
                     cliente.get(i).getNome(),
                     cliente.get(i).getCpf(),
                     cliente.get(i).getRg(),
+                    cliente.get(i).getCelular(),
                     cliente.get(i).getEndereco(),
                     cliente.get(i).getCidade(),
                     cliente.get(i).getEstado()
@@ -60,17 +64,33 @@ public class GuiBuscarClientes extends javax.swing.JInternalFrame {
 
         tabela_Cliente.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null}
             },
             new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
+                "ID", "NOME", "CPF", "RG", "CELULAR", "ENDEREÇO", "CIDADE", "ESTADO"
             }
-        ));
+        ) {
+            Class[] types = new Class [] {
+                java.lang.Integer.class, java.lang.Object.class, java.lang.Object.class, java.lang.Object.class, java.lang.Object.class, java.lang.Object.class, java.lang.Object.class, java.lang.Object.class
+            };
+            boolean[] canEdit = new boolean [] {
+                false, false, false, false, false, false, false, false
+            };
+
+            public Class getColumnClass(int columnIndex) {
+                return types [columnIndex];
+            }
+
+            public boolean isCellEditable(int rowIndex, int columnIndex) {
+                return canEdit [columnIndex];
+            }
+        });
         jScrollPane1.setViewportView(tabela_Cliente);
 
+        jb_Buscar.setFont(new java.awt.Font("Tahoma", 0, 18)); // NOI18N
         jb_Buscar.setText("Buscar");
         jb_Buscar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
@@ -82,20 +102,23 @@ public class GuiBuscarClientes extends javax.swing.JInternalFrame {
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 744, Short.MAX_VALUE)
             .addGroup(layout.createSequentialGroup()
-                .addGap(48, 48, 48)
-                .addComponent(jb_Buscar)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap()
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 724, Short.MAX_VALUE)
+                .addContainerGap())
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jb_Buscar, javax.swing.GroupLayout.PREFERRED_SIZE, 157, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(290, 290, 290))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addGap(111, 111, 111)
-                .addComponent(jb_Buscar)
-                .addGap(48, 48, 48)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 262, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGap(25, 25, 25)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 298, Short.MAX_VALUE)
+                .addGap(40, 40, 40)
+                .addComponent(jb_Buscar, javax.swing.GroupLayout.PREFERRED_SIZE, 59, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(32, 32, 32))
         );
 
         pack();
