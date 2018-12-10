5ac46bcdeba9a9e08f8ae7bf7a0ae7abd2b0a491
michaelJordan1992 <michael_jordan@outlook.com.br>
2018-12-10 17:12:24 -0200

    Cliente conexão com banco de dados

--- src/DAO/DAOCliente.java
+++ src/DAO/DAOCliente.java
@@ -75,7 +75,7 @@ public class DAOCliente {
         
     }
     public ArrayList<Cliente> buscarClienteCpf(Cliente c) throws Exception{
-        sql = "SELECT * FROM CLIENTE WHERE CPF'"+c.getCpf()+"'";
+        sql = "SELECT * FROM CLIENTE WHERE CPF ='"+c.getCpf()+"' ";
         ResultSet rs = stm.executeQuery(sql);
         
         ArrayList<Cliente> cliente = new ArrayList<>();
@@ -92,25 +92,24 @@ public class DAOCliente {
             c1.setCidade(rs.getString("CIDADE"));
             c1.setEstado(rs.getString("ESTADO"));
             
-            cliente.add(c);
+            cliente.add(c1);
+            
         }
         stm.close();
         con.close();
         return cliente;
     }
+   
     public void atualizarClienteCpf(Cliente c) throws Exception{
-        sql = "UPDATE CLIENTE SET NOME '"+c.getNome()+"',CPF'"+c.getCpf()+"'"
-                + ",RG'"+c.getRg()+"',CELULAR'"+c.getCelular()+"'"
-                + ",ENDERECO'"+c.getEndereco()+"',CIDADE'"+c.getCidade()+"',ESTADO'"+c.getEstado()+","
-                + "WHERE CPF '"+c.getCpf()+"'";
+        sql = "UPDATE CLIENTE SET  NOME = '"+c.getNome()+"',CPF = '"+c.getCpf()+"'"
+                + ",RG = '"+c.getRg()+"',CELULAR = '"+c.getCelular()+"'"
+                + ",ENDERECO = '"+c.getEndereco()+"',CIDADE = '"+c.getCidade()+"',ESTADO = '"+c.getEstado()+"'"
+                + "WHERE CPF = '"+c.getCpf()+"' ";
+        
         stm.executeUpdate(sql);
         stm.close();
         con.close();
         
     }
 }
-     
-
-    
-    
 
